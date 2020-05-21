da82d93154eaa0c5cfa37ee151ff240481d949bf
vishnu <vishnu31122013@gmail.com>
2020-05-21 13:41:58 +0530

    gc-1 records loaded to global cache as string

--- GlobalCacheDemo_V1_JP/src/com/wordpress/code2blog/esql_interface/PutEmployee2Cache.java
+++ GlobalCacheDemo_V1_JP/src/com/wordpress/code2blog/esql_interface/PutEmployee2Cache.java
@@ -2,6 +2,8 @@ package com.wordpress.code2blog.esql_interface;
 
 import java.io.StringWriter;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -11,19 +13,32 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Node;
 
 import com.ibm.broker.plugin.MbElement;
+import com.ibm.broker.plugin.MbGlobalMap;
+import com.wordpress.code2blog.EMPLOYEE;
 
 public class PutEmployee2Cache {
-	public static void loadDbRow(MbElement dbRow){
-		try{
+	public static void loadDbRow(MbElement dbRow) {
+		try {
 			Node domNode = dbRow.getDOMNode();
 			String xmlString = domToString(domNode);
 			xmlString.length();// stop debug here
-		}catch(Exception e){
+
+			JAXBContext jaxbContext = JAXBContext.newInstance("com.wordpress.code2blog");
+			EMPLOYEE dBEmployee = ((JAXBElement<EMPLOYEE>) jaxbContext.createUnmarshaller() //
+					.unmarshal(domNode, EMPLOYEE.class)).getValue(); // xsd validation built-in
+			int primaryKey = dBEmployee.getEMPNO();
+
+			// Establish connection with Map called "DemoMap"
+			MbGlobalMap globalMap = MbGlobalMap.getGlobalMap("DemoMap");
+			// don't save java objects to map. Save string or bytes
+			globalMap.put(primaryKey, xmlString);
+			return;
+		} catch (Exception e) {
 			// let this error be handled by message flow
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public static String domToString(Node domNode) throws Exception {
 		DOMSource domSource = new DOMSource(domNode);
 		StringWriter writer = new StringWriter();
@@ -36,4 +51,3 @@ public class PutEmployee2Cache {
 		return xmlString;
 	}
 }
-
